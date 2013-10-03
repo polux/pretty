b0f21682c896937eb9b52caabca847db891849e6
Paul Brauner <polux@google.com>
2013-10-03 08:19:58 +0200

    inhibit hints in dartanalyzer, for now

--- tool/run_all_tests.sh
+++ tool/run_all_tests.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-dartanalyzer $ROOT_DIR/lib/*.dart \
-&& dartanalyzer $ROOT_DIR/test/*.dart \
-&& dartanalyzer $ROOT_DIR/example/*.dart \
+dartanalyzer --no-hints $ROOT_DIR/lib/*.dart \
+&& dartanalyzer --no-hints $ROOT_DIR/test/*.dart \
+&& dartanalyzer --no-hints $ROOT_DIR/example/*.dart \
 && dart $ROOT_DIR/test/pretty_test.dart --quiet
