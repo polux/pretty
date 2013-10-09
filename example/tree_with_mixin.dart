dbdf7f3bc685a15279b97a428ba12db249607469
Rafael de Andrade <rafa.bra@gmail.com>
2013-10-08 22:20:30 -0300

    Created tree_with_mixin.dart.
    This file contains the exact same Tree class of tree.dart, but now using the functionality provided by the pretty_extra library.

---
+++ example/tree_with_mixin.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2013, Google Inc. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+// Author: Paul Brauner (polux@google.com)
+//         Rafael de Andrade (rafa.bra@gmail.com)
+
+import 'package:pretty/pretty.dart';
+import 'package:pretty/pretty_extras.dart';
+
+class Tree extends Object with Pretty {
+  final String name;
+  final List<Tree> children;
+
+  Tree(this.name, this.children);
+
+  Document get pretty =>
+    tree(name, children.map((Tree t) => t.group), indentation: 2);
+}
+
+final Tree someTree = new Tree("aaa",
+    [new Tree("bbbbb",
+        [new Tree("ccc", []),
+         new Tree("dd", [])]),
+    new Tree("eee", []),
+    new Tree("ffff",
+        [new Tree("gg", []),
+         new Tree("hhh", []),
+         new Tree("ii", [])])]);
+
+void main() {
+  for (int width in [100, 50, 20, 10]) {
+    print(someTree.group.render(width));
+    print("");
+  }
+}
